3b6ff143ea022211667989618c59fc103d9e5074
Rutuja1923 <rutujajirage19@gmail.com>
2024-11-04 06:52:55 +0530

    function to shuffle values of an array

--- script.js
+++ script.js
@@ -155,3 +155,10 @@ function generateCategoryDiv() {
 //call the function to generate 
 generateCategoryDiv();
 
+//function to shuffle the array containing options
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+};
